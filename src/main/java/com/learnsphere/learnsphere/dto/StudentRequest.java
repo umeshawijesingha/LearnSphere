77146eb7348808fb3cc2114332a60fc6cddb88db
umeshawijesingha <umeshawijesingha@gmail.com>
2025-11-19 23:47:41 +0530

    Add validation for first name, email, and contact number in student request DTO

--- src/main/java/com/learnsphere/learnsphere/dto/StudentRequest.java
+++ src/main/java/com/learnsphere/learnsphere/dto/StudentRequest.java
@@ -1,6 +1,9 @@
 package com.learnsphere.learnsphere.dto;
 
 import com.learnsphere.learnsphere.enums.StudentStatus;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,14 +12,25 @@ import java.util.Date;
 @Getter
 @Setter
 public class StudentRequest {
-    private String keycloakUserId;
+
+    @NotBlank(message = "First name is required")
+    @Pattern(regexp = ".*[A-Za-z]].*", message = "First letter must contain at least one letter")
     private String firstname;
+
     private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
     private String address;
     private String city;
     private String school;
+
+    @NotBlank(message = "Contact number is required")
+    @Pattern(regexp = "^(?:\\\\+94|0)?7\\\\d{8}$")
     private String contactNo;
+
     private String nicUrl;
     private String profileUrl;
     private Date dateOfBirth;
