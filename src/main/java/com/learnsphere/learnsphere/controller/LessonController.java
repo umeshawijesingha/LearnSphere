b1d37e097636c7273c4dcf735a0ce63dda614162
umeshawijesingha <umeshawijesingha@gmail.com>
2025-12-02 23:19:26 +0530

    Implemented Lesson endpoints

--- src/main/java/com/learnsphere/learnsphere/controller/LessonController.java
+++ src/main/java/com/learnsphere/learnsphere/controller/LessonController.java
@@ -1,9 +1,52 @@
 package com.learnsphere.learnsphere.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.learnsphere.learnsphere.dto.LessonRequest;
+import com.learnsphere.learnsphere.dto.LessonResponse;
+import com.learnsphere.learnsphere.entity.Lesson;
+import com.learnsphere.learnsphere.mapper.LessonMapper;
+import com.learnsphere.learnsphere.service.LessonService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/lessons")
 public class LessonController {
+
+    private LessonService lessonService;
+    private LessonMapper lessonMapper;
+
+    @GetMapping
+    public ResponseEntity<List<LessonResponse>> getAllLessons() {
+        List<Lesson> lessons = lessonService.getAllLessons();
+        List<LessonResponse> response = lessons.stream()
+                .map(lessonMapper::toResponse).toList();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<LessonResponse> createLesson(@RequestBody LessonRequest request) {
+        Lesson lesson = lessonMapper.toEntity(request);
+        lesson = lessonService.createLesson(lesson);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(lessonMapper.toResponse(lesson));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LessonResponse> updateLesson(@PathVariable Long id, @RequestBody LessonRequest request) {
+        Lesson lesson = lessonMapper.toEntity(request);
+        lesson = lessonService.updateLesson(id, lesson);
+
+        return ResponseEntity.ok(lessonMapper.toResponse(lesson));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLesson(@PathVariable Long id) {
+        lessonService.deleteCourse(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/learnsphere/learnsphere/service/LessonService.java
+++ src/main/java/com/learnsphere/learnsphere/service/LessonService.java
@@ -1,6 +1,7 @@
 package com.learnsphere.learnsphere.service;
 
 import com.learnsphere.learnsphere.entity.Lesson;
+import com.learnsphere.learnsphere.exception.ResourceNotFoundException;
 import com.learnsphere.learnsphere.repository.LessonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,31 @@ public class LessonService {
     @Autowired
     private LessonRepository lessonRepository;
 
-    public List<Lesson> getAllLessons(){
+    public List<Lesson> getAllLessons() {
         return lessonRepository.findAll();
+    }
+
+    public Lesson createLesson(Lesson lesson) {
+        return lessonRepository.save(lesson);
+    }
+
+    public Lesson updateLesson(Long id, Lesson lesson) {
+        Lesson existingLesson = lessonRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Lesson not found with id: " + id));
+
+        //Update the fields
+        existingLesson.setCourse(lesson.getCourse());
+        existingLesson.setOrderIndex(lesson.getOrderIndex());
+        existingLesson.setDuration(lesson.getDuration());
+        existingLesson.setDescription(lesson.getDescription());
+        existingLesson.setTitle(lesson.getTitle());
+        existingLesson.setContentType(lesson.getContentType());
+        existingLesson.setContentUrl(lesson.getContentUrl());
+
+        return lessonRepository.save(existingLesson);
+    }
+
+    public void deleteCourse(Long id) {
+        lessonRepository.deleteById(id);
     }
 }
