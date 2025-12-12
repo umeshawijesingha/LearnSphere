4d7128ded78913df50ac0579032f3ad6661cd0ea
umeshawijesingha <umeshawijesingha@gmail.com>
2025-12-12 23:46:44 +0530

    send email notification after successful enrollment

---
+++ src/main/java/com/learnsphere/learnsphere/service/MailService.java
@@ -0,0 +1,28 @@
+package com.learnsphere.learnsphere.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail, String subject, String body){
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(toEmail);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+
+        }catch(MailException e){
+            throw new RuntimeException("Email sending failed...", e);
+        }
+
+    }
+}
--- src/main/java/com/learnsphere/learnsphere/service/PaymentService.java
+++ src/main/java/com/learnsphere/learnsphere/service/PaymentService.java
@@ -1,35 +1,78 @@
 package com.learnsphere.learnsphere.service;
 
+import com.learnsphere.learnsphere.entity.Course;
+import com.learnsphere.learnsphere.entity.Enrollment;
 import com.learnsphere.learnsphere.entity.Payment;
+import com.learnsphere.learnsphere.entity.Student;
+import com.learnsphere.learnsphere.enums.EnrollmentCompletionStatus;
+import com.learnsphere.learnsphere.enums.PaymentStatus;
+import com.learnsphere.learnsphere.enums.PaymentType;
 import com.learnsphere.learnsphere.exception.ResourceNotFoundException;
+import com.learnsphere.learnsphere.repository.CourseRepository;
+import com.learnsphere.learnsphere.repository.EnrollmentRepository;
 import com.learnsphere.learnsphere.repository.PaymentRepository;
+import com.learnsphere.learnsphere.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Transactional
 @Service
 public class PaymentService {
 
     @Autowired
     private PaymentRepository paymentRepository;
 
-    public List<Payment> getAllPayments(){
+    @Autowired
+    private EnrollmentService enrollmentService;
+
+    @Autowired
+    private StudentService studentService;
+
+    @Autowired
+    private CourseService courseService;
+
+    @Autowired
+    private MailService mailService;
+
+    @Autowired
+    private RefundService refundService;
+
+    public List<Payment> getAllPayments() {
         return paymentRepository.findAll();
     }
 
-    public Payment getPaymentById(Long id){
+    public Payment getPaymentById(Long id) {
         return paymentRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Payment not found with Id:" +id));
+                .orElseThrow(() -> new ResourceNotFoundException("Payment not found with Id:" + id));
     }
 
-    public Payment createPayment(Payment payment){
+    public Payment createPayment(Payment payment) {
+        payment.setType(PaymentType.PAYMENT);
+        Long studentId = payment.getStudent().getId();
+        Student student = studentService.getStudentByID(studentId);
+
+        Long courseId = payment.getCourse().getId();
+        Course course = courseService.getCourseById(courseId);
+
+        Enrollment enrollment = new Enrollment();
+        enrollment.setCompletionStatus(EnrollmentCompletionStatus.COMPLETED);
+        enrollment.setStudent(payment.getStudent());
+        enrollment.setCourse(payment.getCourse());
+        enrollment.setEnrolledAt(LocalDateTime.now());
+        enrollmentService.createEnrollment(enrollment);
+        mailService.sendEmail(payment.getStudent().getEmail(),
+                "LearnSphere",
+                "You are enrolled to course:" + enrollment.getCourse().getTitle());
         return paymentRepository.save(payment);
     }
 
-    public Payment updatePayment(Long id, Payment payment){
+    public Payment updatePayment(Long id, Payment payment) {
         Payment existingPayment = paymentRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Payment not found with Id:" +id));
+                .orElseThrow(() -> new ResourceNotFoundException("Payment not found with Id:" + id));
 
         existingPayment.setStatus(payment.getStatus());
         existingPayment.setUpdateAt(payment.getUpdateAt());
